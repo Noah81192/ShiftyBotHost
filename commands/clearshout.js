46053ca720ab840cf804baff8322750dec75d770
Noah81192 <66043561+Noah81192@users.noreply.github.com>
2020-11-09 14:12:03 -0500

    Update clearshout.js

--- commands/clearshout.js
+++ commands/clearshout.js
@@ -1,51 +1,119 @@
-const roblox = require('noblox.js');
-const chalk = require('chalk');
-require('dotenv').config();
+const roblox = require("noblox.js");
+const discord = require("discord.js");
 
-exports.run = async (client, message, args) => {
-    if(!message.member.roles.cache.some(role =>["Ranking Permissions", "Shout Permissions"].includes(role.name))){
-        return message.channel.send({embed: {
-            color: "#ff4040",
-            description: "You need the `Ranking Permissions` or `Shout Permissions` role to run this command.",
-            author: {
-                name: message.author.tag,
-                icon_url: message.author.displayAvatarURL()
+exports.run = async (client, message, args, config) => {
+  if (message.member.roles.cache.has(config.rankperm)) {
+    let discorduser = message.author.tag;
+    let shout = "ttly";
+
+    const rankbed2 = new discord.MessageEmbed()
+      .setTitle("Roblox ranking FAILED")
+      .setColor("RED")
+
+      .setDescription(
+        `Error: There is no shout, or the shout could not be found`
+      )
+      .setFooter(config.embed.footer + " | Ranking command");
+
+    const rankbed3 = new discord.MessageEmbed()
+      .setTitle("Roblox ranking.")
+      .setColor("RED")
+
+      .setDescription(`Conecting to roblox group...`)
+      .setFooter(config.embed.footer + " | Ranking command");
+
+    const rankbigerror = new discord.MessageEmbed()
+      .setTitle("Roblox ranking FAILED")
+      .setColor("RED")
+
+      .setDescription(
+        `Shouting has failed, We are not sure the reason, Tell the owner of the bot to check logs`
+      )
+      .setFooter(config.embed.footer + " | Ranking command");
+
+    const rankbed4 = new discord.MessageEmbed()
+      .setTitle("💡 Roblox shouting.")
+      .setColor("GREEN")
+
+      .setDescription(`💡 Clearing... Please wait.`)
+      .setFooter(config.embed.footer + " | Ranking command");
+
+    const canceled = new discord.MessageEmbed()
+      .setTitle("Ranking canceled.")
+      .setColor("RED")
+
+      .setDescription(`We have canceled the clear shout command.`)
+      .setFooter(config.embed.footer + " | Ranking command");
+
+    const origshout = await roblox.getShout(config.groupid);
+
+    const rankdogjfhfk = new discord.MessageEmbed()
+      .setTitle("Ranking confermation")
+      .setColor("GREEN")
+      .setDescription(
+        `We have connected to the roblox group service, Are you sure you would like to clear the shout?`
+      )
+      .addField("Current shout message:", origshout.body, true);
+
+    const msg = await message.channel.send(rankbed3);
+
+    if (shout) {
+      msg.edit(rankdogjfhfk);
+      msg.react("✅").then(() => msg.react("❌"));
+
+      const filter = (reaction, user) => {
+        return (
+          ["✅", "❌"].includes(reaction.emoji.name) &&
+          user.id === message.author.id
+        );
+      };
+
+      msg
+        .awaitReactions(filter, { max: 1, time: 60000, errors: ["time"] })
+        .then((collected) => {
+          const reaction = collected.first();
+          if (reaction.emoji.name === "✅") {
+            msg.reactions
+              .removeAll()
+              .catch((error) =>
+                console.error("Failed to clear reactions: ", error)
+              );
+            msg.edit(rankbed4);
+            roblox
+              .shout({
+                group: config.groupid,
+                message: "",
+              })
+              .catch(function (err) {
+                msg.edit(rankbigerror);
+                console.log(err);
+              });
+            const rankeddrfugsd = new discord.MessageEmbed()
+              .setTitle(":tada: Clear Shout success!")
+              .setColor("GREEN")
+              .setDescription(`We have cleared the shout on your group!`)
+              .addField("Old shout message:", origshout.body, true)
+              .setFooter(config.embed.footer + " | Ranking command");
+            msg.edit(rankeddrfugsd);
+            if (config.robloxlogchannel === "true") {
+              const ranklog = new discord.MessageEmbed()
+                .setTitle("Ranking action.")
+                .setColor("GREEN")
+                .setDescription(`There has been a ranking action.`)
+                .addField("Action type:", "Clear shout", true)
+
+                .addField("Old shout:", origshout.body, true)
+
+                .addField("Ranker:", discorduser, true)
+                .setFooter(config.embed.footer + " | Ranking command");
+              message.guild.channels.cache
+                .get(config.robloxlogchannelid)
+                .send(ranklog);
             }
-        }});
+          } else {
+            msg.edit(canceled);
+          }
+        });
     }
-    let clearShoutResponse;
-    try {
-        clearShoutResponse = await roblox.shout(Number(process.env.groupId), '');
-    } catch (err) {
-        console.log(chalk.red('An error occured when running the clearshout command: ' + err));
-        return message.channel.send({embed: {
-            color: "#f7ff7a",
-            description: `Oops! An unexpected error has occured. It has been logged to the bot console.`,
-            author: {
-                name: message.author.tag,
-                icon_url: message.author.displayAvatarURL()
-            }
-        }});
-    }
-    message.channel.send({embed: {
-        color: "#404dff",
-        description: `**Success!** Cleared group shout.`,
-        author: {
-            name: message.author.tag,
-            icon_url: message.author.displayAvatarURL()
-        }
-    }});
-     channel = client.channels.cache.get(process.env.logchannelid);
-    channel.send({embed: {
-        color: "#404dff",
-        description: `<@${message.author.id}> has cleared the group shout.`,
-        author: {
-            name: message.author.tag,
-            icon_url: message.author.displayAvatarURL()
-        },
-        footer: {
-            text: 'Action Logs'
-        },
-        timestamp: new Date()
-    }});
-}
+  }
+};
